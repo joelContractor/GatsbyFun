07d47e86670017979ef4cc78c411175dbc941d26
joelContractor <87203562+joelContractor@users.noreply.github.com>
2022-02-13 01:00:54 +0000

    updated sitegallery

--- Pebbles/src/components/siteGallery.js
+++ Pebbles/src/components/siteGallery.js
@@ -3,7 +3,7 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { Grid } from "@material-ui/core";
-
+import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
@@ -15,28 +15,32 @@ function SiteGallery({ data }) {
         
 
  
-            <Grid container direction="row" spacing={2} sx={{ fontSize:`13px`, fontFamily:`Arial`}}>
+            <Grid container direction="row" spacing={3} sx={{ fontSize:`13px`, fontFamily:`Arial`}}>
             
                 
                         {data.map((dats) => (
                                
-                            <Grid item xs={6} sm={6} md={4} lg={3} state={dats} >
+                            <Grid item xs={6} sm={6} md={4} lg={4} state={dats} >
                                 
-                                <Card sx={{ boxShadow: '0px 0px 0px 0px rgba(0,0,0,0.2),0px 0px 0px 0px rgba(0,0,0,0.14),0px 1px 3px 0px rgba(0,0,0,0.12)', backgroundColor: '#f9f9f9', border:"1px solid white", padding:"0px"}}>
+                                <Card sx={{ padding:"2px", height:"425px"}}>
+                                    <Button href={dats.node.agencyurl} target="_blank" style={{ textTransform: `none`, color:"white", fontWeight:"normal", fontSize:"12px", width:"100%", textAlign:"center", border: "1px white solid", marginBottom:"0px", background: "rgba(0, 0, 0, 0.9)", padding: "20px 10px 15px 15px", borderBottomRightRadius: "0px", borderBottomLeftRadius:"0px", borderTopLeftRadius: "5px", borderTopRightRadius: "5px" }}>
+                                        {dats.node.agencytitle}
+                                    </Button>
                                     <CardMedia
                                         component="img"
                                         height="200"
                                         image={dats.node.agencyimage.url}
                                         alt={dats.node.agencytitle}
-                                        sx={{  }}
+                                        sx={{ mb:"0px", border: "1px solid white", borderTop:"0px" }}
                                     />
-                                    <CardContent>
-                                        <p>
-                                            <a href={dats.node.agencyurl} target="_blank" rel="noreferrer" role="button">{dats.node.agencytitle}</a>
-                                        </p>
-                                        <p style={{fontFamily: 'Arial', fontSize:"12px", color: "rgba(0, 0, 0, 0.6)"}}>
+                                    <CardContent
+                                        sx={{ background:"white", borderBottomLeftRadius: "3px", borderBottomRightRadius: "3px" }}
+                                    >
+                                        
+                                        <p style={{fontFamily: 'Arial', fontSize:"12px", color: "rgba(0, 0, 0, 0.6)", marginBottom:"0px"}}>
                                             {dats.node.agencydescription}
                                         </p>
+                                        
                                     </CardContent>
                                     {/*<CardActions sx={{justifyContent:'center', marginBottom:'8px', pt:"0px"}}>
                                         
