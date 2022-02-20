adeb81e3c1364b9a9e6686dfdcfe5fd10541e8c8
joelContractor <87203562+joelContractor@users.noreply.github.com>
2022-02-20 11:54:28 +0000

    added responsive nav

--- Pebbles/src/components/header.js
+++ Pebbles/src/components/header.js
@@ -1,10 +1,32 @@
-import * as React from "react"
+import React, { useState, useEffect } from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import { styled } from "@mui/system";
 
 
-const Header = ({ siteTitle }) => (
+
+function Header ({ siteTitle }) {
+
+  const [matches, setMatches] = useState(
+    window.matchMedia("(min-width: 768px)").matches
+  )
+  
+  useEffect(() => {
+    window
+    .matchMedia("(min-width: 768px)")
+    .addEventListener('change', e => setMatches( e.matches ));
+  }, []);
+
+  const [show, setShow] = useState('none')
+
+  function showInfo(){
+      if(show ==='none'){
+          setShow('block');
+      }else{setShow('none');}
+  }
+
+  const res = 
   <header style={ {float:`left`, background: `#1A2421`, marginBottom: `0rem`, width:`100%`} }>
     <div style={ {margin: `0 auto`, maxWidth: 900, padding: `1.45rem 1.0875rem`,} }>
       <div style={{ float:`left`, marginRight:`5px`, marginTop:"3px" }}>
@@ -21,13 +43,22 @@ const Header = ({ siteTitle }) => (
         <Link to="/" style={ {float:`left`, color: `white`, textDecoration: `none`, textShadow:`2px 1px black`, marginTop:'5px'} }> {siteTitle} </Link>
       </p>
       <p style={{ float:`right`, textAlign:`right`, marginTop:"7px", fontSize: '14px' }}>
-        <Link to="/artist/" activeStyle={{ color: "red" }} style={ {color: `#fff`,textDecoration: `none`, textShadow:`2px 1px black`} }> actors </Link>
+        
+        {matches && (
+          <><Link to="/artist/" activeStyle={{ color: "red" }} style={{ color: `#fff`, textDecoration: `none`, textShadow: `2px 1px black` }}> actors </Link><Link to="/writer/" activeStyle={{ color: "red" }} style={{ color: `#fff`, textDecoration: `none`, textShadow: `2px 1px black` }}> writers </Link><Link to="/director/" activeStyle={{ color: "red" }} style={{ color: `#fff`, textDecoration: `none`, textShadow: `2px 1px black` }}> directors </Link></>
+        )}
+        {!matches && (<button className="mobile" onClick={showInfo}>mobile</button>)}
+      </p>
+      <p style={{ clear:"both", textAlign:`center`, marginTop:"0px", marginBottom:"0.5rem", fontSize: '14px', display:show === 'none'? `none`: `block` }}>
+        <Link to="/artist/" activeStyle={{ color: "red" }} style={ {color: `#fff`,textDecoration: `none`, textShadow:`2px 1px black`, paddingRight:"3rem"} }> actors </Link>
         <Link to="/writer/" activeStyle={{ color: "red" }} style={ {color: `#fff`, textDecoration: `none`, textShadow:`2px 1px black`} }> writers </Link>
-        <Link to="/director/" activeStyle={{ color: "red" }} style={ {color: `#fff`, textDecoration: `none`, textShadow:`2px 1px black`} }> directors </Link>
+        <Link to="/director/" activeStyle={{ color: "red" }} style={ {color: `#fff`, textDecoration: `none`, textShadow:`2px 1px black`, paddingLeft:"3rem"} }> directors </Link>
       </p>
     </div>
   </header>
-)
+
+  return res;
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
