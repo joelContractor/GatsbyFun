61308f50d8465dcffd4f10c7c045465b6980249c
joelContractor <87203562+joelContractor@users.noreply.github.com>
2022-02-20 22:41:39 +0000

    why did I do this?

--- Pebbles/src/components/header.js
+++ Pebbles/src/components/header.js
@@ -20,7 +20,7 @@ function Header ({ siteTitle }) {
   const res = 
   <header style={ {float:`left`, background: `#1A2421`, marginBottom: `0rem`, width:`100%`} }>
     <div style={ {margin: `0 auto`, maxWidth: 900, padding: `1.45rem 1.0875rem`} }>
-      <div style={{ float:`left`, marginRight:`5px`, marginTop:"3px" }}>
+      <div style={{ float:`left`, marginRight:`5px`, marginTop:"3px", width:"30px" }}>
         <StaticImage
             src="../images/diverseGenderIcon.png"
             width={30}
@@ -32,7 +32,7 @@ function Header ({ siteTitle }) {
       </div>
         <Link to="/" style={ {float:`left`, color: `white`, textDecoration: `none`, textShadow:`2px 1px black`, marginTop:'5px'} }> {siteTitle} </Link>
  
-      <p style={{ float:`right`, textAlign:`right`, marginTop:"5px", fontSize: '1rem', paddingRight:"0.5rem" }}>
+      <div style={{ float:`right`, textAlign:`right`, marginTop:"5px", fontSize: '1rem', paddingRight:"0.5rem", paddingBottom:"1.4rem" }}>
       { matches && <><Link to="/artist/" activeStyle={{ color: "red" }} style={ {color: `#fff`, textDecoration: `none`, textShadow:`2px 1px black`, paddingRight:"1rem" } }> actors </Link> 
         <Link to="/writer/" activeStyle={{ color: "red" }} style={ {color: `#fff`, textDecoration: `none`, textShadow:`2px 1px black`} }> writers </Link>
         <Link to="/director/" activeStyle={{ color: "red" }} style={ {color: `#fff`, textDecoration: `none`, textShadow:`2px 1px black`, paddingLeft:"1rem"} }> directors </Link></> 
@@ -42,7 +42,7 @@ function Header ({ siteTitle }) {
         <div className="mobileNav" style={{ border:"1px solid white", background:"white", borderRadius:"5px", width:"30px", marginTop:"8px" }}></div>
         <div className="mobileNav" style={{ border:"1px solid white", background:"white", borderRadius:"5px", width:"30px", marginTop:"8px" }}></div>
       </button> }
-      </p>
+      </div>
       <div style={{ clear:"both", textAlign:`right`, paddingTop:"5px", marginBottom:"0rem", marginRight:"4px", fontSize: '1rem', display:show === 'none'? `none`: `block` }}>
         <Link to="/artist/" activeStyle={{ color: "red" }} style={ {padding:"10px 5px 10px 5px", borderRight:"1px dotted white", color: `#fff`,textDecoration: `none`, textShadow:`2px 1px black`, paddingRight:"1rem"} }> actors </Link>
         <Link to="/writer/" activeStyle={{ color: "red" }} style={ {padding:"10px 5px 10px 5px", borderRight:"1px dotted white", color: `#fff`, textDecoration: `none`, textShadow:`2px 1px black`, paddingLeft:"1rem", paddingRight:"1rem"} }> writers </Link>
