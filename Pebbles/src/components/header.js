cd2aa5c3219b1756a035a088751371b2643b341c
joelContractor <87203562+joelContractor@users.noreply.github.com>
2022-02-20 22:07:50 +0000

    Update header.js

--- Pebbles/src/components/header.js
+++ Pebbles/src/components/header.js
@@ -43,10 +43,10 @@ function Header ({ siteTitle }) {
         <div className="mobileNav" style={{ border:"1px solid white", background:"white", borderRadius:"5px", width:"30px", marginTop:"8px" }}></div>
       </button> }
       </p>
-      <div style={{ clear:"both", textAlign:`right`, paddingTop:"5px", marginBottom:"0rem", marginRight:"4px", fontSize: '1.1rem', display:show === 'none'? `none`: `block` }}>
-        <Link to="/artist/" activeStyle={{ color: "red" }} style={ {padding:"15px 5px 15px 5px", borderRight:"1px dotted white", color: `#fff`,textDecoration: `none`, textShadow:`2px 1px black`, paddingRight:"1rem"} }> actors </Link>
-        <Link to="/writer/" activeStyle={{ color: "red" }} style={ {padding:"15px 5px 15px 5px", borderRight:"1px dotted white", color: `#fff`, textDecoration: `none`, textShadow:`2px 1px black`, paddingLeft:"1rem", paddingRight:"1rem"} }> writers </Link>
-        <Link to="/director/" activeStyle={{ color: "red" }} style={ {padding:"15px 5px 15px 5px", color: `#fff`, textDecoration: `none`, textShadow:`2px 1px black`, paddingLeft:"1rem"} }> directors </Link>
+      <div style={{ clear:"both", textAlign:`right`, paddingTop:"5px", marginBottom:"0rem", marginRight:"4px", fontSize: '1rem', display:show === 'none'? `none`: `block` }}>
+        <Link to="/artist/" activeStyle={{ color: "red" }} style={ {padding:"10px 5px 10px 5px", borderRight:"1px dotted white", color: `#fff`,textDecoration: `none`, textShadow:`2px 1px black`, paddingRight:"1rem"} }> actors </Link>
+        <Link to="/writer/" activeStyle={{ color: "red" }} style={ {padding:"10px 5px 10px 5px", borderRight:"1px dotted white", color: `#fff`, textDecoration: `none`, textShadow:`2px 1px black`, paddingLeft:"1rem", paddingRight:"1rem"} }> writers </Link>
+        <Link to="/director/" activeStyle={{ color: "red" }} style={ {padding:"10px 5px 10px 5px", color: `#fff`, textDecoration: `none`, textShadow:`2px 1px black`, paddingLeft:"1rem"} }> directors </Link>
       </div>
     </div>
   </header>
