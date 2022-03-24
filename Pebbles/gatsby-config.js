8069387f38f392294be1a2d2132f9523d686c62d
joelContractor <87203562+joelContractor@users.noreply.github.com>
2022-03-24 01:17:36 +0000

    pwa work

--- Pebbles/gatsby-config.js
+++ Pebbles/gatsby-config.js
@@ -44,8 +44,26 @@ module.exports = {
       },
     },
     `gatsby-plugin-gatsby-cloud`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Diverse Crew",
+        short_name: "Diverse Crew",
+        start_url: "/",
+        background_color: "#fff",
+        theme_color: "#6b37bf",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/icon.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
