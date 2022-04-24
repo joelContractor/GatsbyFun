2fbcf3af34588c9d866e48ad4edd642c51d2c494
joelContractor <87203562+joelContractor@users.noreply.github.com>
2022-04-24 09:59:41 +0100

    Update userPhysicalAttributes.js

--- Pebbles/src/components/elements/userPhysicalAttributes.js
+++ Pebbles/src/components/elements/userPhysicalAttributes.js
@@ -14,7 +14,8 @@ const StyledUserPhysicalAttributesGrid = styled(Grid, {
     borderTop:"1px dotted grey",
     ".userPhysicalAttributesImage": { willChange:"transform", borderRadius:"5px" },
     ".grey": { color:"rgba(0,0,0,0.6)", fontSize:"0.7rem" },
-    ".titleColor": { color:"hsla(0, 0%, 0%, 0.8)", marginBottom:"0" }
+    ".titleColor": { color:"hsla(0, 0%, 0%, 0.8)", marginBottom:"0" },
+    ".subHeadedVal": { fontSize:"0.6rem" }
 
   });
 
@@ -36,14 +37,14 @@ function UserPhysicalAttributes({ dats }) {
                         <Grid item xs={12} sm={4} container className="grey">
                                 
                                 
-                                <Grid item xs={6} sm={12}><p>Ethnicity: {dats.node.ethnicity}</p></Grid>
-                                <Grid item xs={6} sm={12}><p>Height: {dats.node.height}</p></Grid>
-                                <Grid item xs={6} sm={12}><p>Weight: {dats.node.weight}</p></Grid>
-                                <Grid item xs={6} sm={12}><p>Eye Colour: {dats.node.eyeColour}</p></Grid>
-                                <Grid item xs={6} sm={12}><p>Tatoos: {dats.node.tatoos}</p></Grid>
-                                <Grid item xs={6} sm={12}><p>Visible Piercings: {dats.node.visiblepiercings}</p></Grid>
-                                <Grid item xs={6} sm={12}><p>Hair Length: {dats.node.hairLength}</p></Grid>
-                                <Grid item xs={12} sm={12}><p>Hair Colour: {dats.node.hairColour}</p></Grid>
+                                <Grid item xs={6} sm={12}><p><span className="subHeadedVal">Ethnicity:</span> {dats.node.ethnicity}</p></Grid>
+                                <Grid item xs={6} sm={12}><p><span className="subHeadedVal">Height:</span> {dats.node.height}</p></Grid>
+                                <Grid item xs={6} sm={12}><p><span className="subHeadedVal">Weight:</span> {dats.node.weight}</p></Grid>
+                                <Grid item xs={6} sm={12}><p><span className="subHeadedVal">Eye Colour:</span> {dats.node.eyeColour}</p></Grid>
+                                <Grid item xs={6} sm={12}><p><span className="subHeadedVal">Tatoos:</span> {dats.node.tatoos}</p></Grid>
+                                <Grid item xs={6} sm={12}><p><span className="subHeadedVal">Visible Piercings:</span> {dats.node.visiblepiercings}</p></Grid>
+                                <Grid item xs={6} sm={12}><p><span className="subHeadedVal">Hair Length:</span> {dats.node.hairLength}</p></Grid>
+                                <Grid item xs={12} sm={12}><p><span className="subHeadedVal">Hair Colour:</span> {dats.node.hairColour}</p></Grid>
                             
                         </Grid></>
                     }
