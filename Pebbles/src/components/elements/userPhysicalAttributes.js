d385e52f352f732f87bcad718a326600601fdb42
joelContractor <87203562+joelContractor@users.noreply.github.com>
2022-04-24 08:08:41 +0100

    Update userPhysicalAttributes.js

--- Pebbles/src/components/elements/userPhysicalAttributes.js
+++ Pebbles/src/components/elements/userPhysicalAttributes.js
@@ -13,7 +13,7 @@ const StyledUserPhysicalAttributesGrid = styled(Grid, {
     paddingBottom:"50px",
     borderTop:"1px dotted grey",
     ".userPhysicalAttributesImage": { willChange:"transform", borderRadius:"5px" },
-    ".grey": { color:"rgba(0,0,0,0.6)", fontSize:"0.7rem" },
+    ".grey": { color:"rgba(0,0,0,0.6)", fontSize:"0.7rem", paddingTop:"2rem" },
     ".titleColor": { color:"hsla(0, 0%, 0%, 0.8)", marginBottom:"0" }
 
   });
@@ -33,7 +33,7 @@ function UserPhysicalAttributes({ dats }) {
                         return <><Grid item xs={12} className="">
                                 <Grid item xs={12} sm={12}><h2 className="titleColor">Physical Attributes</h2></Grid>
                             </Grid>
-                        <Grid item xs={12} sm={4}  className="grey">
+                        <Grid item xs={12} sm={4} container className="grey">
                                 
                                 
                                 <Grid item xs={6} sm={12}><p>Ethnicity: {dats.node.ethnicity}</p></Grid>
