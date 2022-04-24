305589f52973f6eb27816d601114522bcb8aaa85
joelContractor <87203562+joelContractor@users.noreply.github.com>
2022-04-24 16:22:53 +0100

    Update layout.js

--- Pebbles/src/components/layout.js
+++ Pebbles/src/components/layout.js
@@ -47,26 +47,26 @@ const Layout = ({ children }) => {
         <div style={{ margin: `0 auto`, maxWidth: 900, padding: `0 1.0875rem 1.45rem`, color:"#adadad", fontSize:"0.8rem"}} >
           <Grid container direction="row" spacing={2} style={{ paddingTop:"60px"}}>
 
-            <Grid item xs={12} md={6} style={{ textAlign:"left", fontSize:"0.8rem" }}>
-              <h4 style={{ fontWeight:"normal", width:"90%", margin:"0 auto", paddingBottom:"2rem", fontSize:"0.8rem"}}>Participate</h4>
+            <Grid item xs={12} md={6} style={{ textAlign:"left", fontSize:"0.7rem" }}>
+              <h4 style={{ color:"white", fontWeight:"normal", width:"90%", margin:"0 auto", paddingBottom:"2rem", fontSize:"0.8rem"}}>Participate</h4>
               <p style={{ fontFamily:"Arial", width:"90%", margin:"0 auto", paddingBottom:"2rem"}}>We aim to collate and help highlight diverse talent in the film industry. Be found on this site free! We ONLY feature publicly available contact details used for business (numbers/emails to agents or self-promoting talent). We are not an agency, any contact will be direct from production company to agent/talent.</p>
             </Grid>
 
-            <Grid item xs={12} md={6} style={{ textAlign:"left", fontSize:"0.8rem" }}>
-              <h4 style={{ fontWeight:"normal", width:"90%", margin:"0 auto", paddingBottom:"2rem", fontSize:"0.8rem"}}>Be Aware</h4>
+            <Grid item xs={12} md={6} style={{ textAlign:"left", fontSize:"0.7rem" }}>
+              <h4 style={{ color:"white", fontWeight:"normal", width:"90%", margin:"0 auto", paddingBottom:"2rem", fontSize:"0.8rem"}}>Be Aware</h4>
               <p style={{ fontFamily:"Arial", width:"90%", margin:"0 auto", paddingBottom:"2rem"}}>Under 18's without representation to handle queries are not permitted to be featured on this site. All data featured is publically available. We do not store any private user details. We do not process any data for any reason. Talent featured can request removal or modification of their profile at any time.</p>
             </Grid>
 
-            <Grid item xs={12} md={6} style={{ textAlign:"left", fontSize:"0.8rem" }}>
-              <h4 style={{ fontWeight:"normal", width:"90%", margin:"0 auto", paddingBottom:"2rem", fontSize:"0.8rem"}}>Writer?</h4>
+            <Grid item xs={12} md={6} style={{ textAlign:"left", fontSize:"0.7rem" }}>
+              <h4 style={{ color:"white", fontWeight:"normal", width:"90%", margin:"0 auto", paddingBottom:"2rem", fontSize:"0.8rem"}}>Writer?</h4>
               <p style={{ fontFamily:"Arial", width:"90%", margin:"0 auto", paddingBottom:"2rem"}}>We feature articles written by diverse writers, performers and crew from the TV, Film and Theatre industries - You. Get in touch about any issues you'd like to see discussed, or 'have your say' internationally by having your very own article published in diversecrew.com</p>
             </Grid>
 
-            <Grid item xs={12} md={6} style={{ textAlign:"left", fontSize:"0.8rem" }}>
+            <Grid item xs={12} md={6} style={{ textAlign:"left", fontSize:"0.7rem" }}>
             </Grid>
 
-            <Grid item xs={4} style={{ textAlign:"center", marginTop:"2rem" }}>
-              <h5 style={{ fontWeight:"normal", fontSize:"0.6rem" }}>Text Us</h5>
+            <Grid item xs={4} style={{ textAlign:"center", marginTop:"2rem", borderTop:"1px dotted white", paddingTop:"4rem", borderBottom:"1px dotted white", paddingBottom:"4rem"  }}>
+              <h5 style={{ color:"white", fontWeight:"normal", fontSize:"0.6rem" }}>Text Us</h5>
               <a aria-label={`Send us a text`} href={`sms:07375510650`}>
                                       
                   <StaticImage
@@ -80,8 +80,8 @@ const Layout = ({ children }) => {
               </a>
             </Grid>
 
-            <Grid item xs={4} style={{ textAlign:"center", marginTop:"2rem" }} >
-              <h5 style={{ fontWeight:"normal", fontSize:"0.6rem" }}>Email Us</h5>
+            <Grid item xs={4} style={{ textAlign:"center", marginTop:"2rem", borderTop:"1px dotted white", paddingTop:"4rem", borderBottom:"1px dotted white", paddingBottom:"4rem"  }} >
+              <h5 style={{ color:"white", fontWeight:"normal", fontSize:"0.6rem" }}>Email Us</h5>
               <a aria-label={`Email us`} href={`mailto:diversecrew.com@gmail.com?&subject=DiverseCrewEnquiry&body=Hi Diverse Crew, I'd like a free profile`}>
                                       
                   <StaticImage
@@ -96,8 +96,8 @@ const Layout = ({ children }) => {
               </a>
             </Grid>
 
-            <Grid item xs={4} style={{ textAlign:"center", marginTop:"2rem" }}>  
-              <h5 style={{ fontWeight:"normal", fontSize:"0.6rem" }}>Call Us</h5>
+            <Grid item xs={4} style={{ textAlign:"center", marginTop:"2rem", borderTop:"1px dotted white", paddingTop:"4rem", borderBottom:"1px dotted white", paddingBottom:"4rem" }}>  
+              <h5 style={{ color:"white", fontWeight:"normal", fontSize:"0.6rem" }}>Call Us</h5>
               <a aria-label={`Call Us`} href={`tel:07375510650`}>
                                       
                   <StaticImage
@@ -113,7 +113,7 @@ const Layout = ({ children }) => {
             </Grid>
 
             <Grid item xs={12} >
-              <p style={{ textAlign:"center", padding:"100px 0px 0px 0px", fontSize:"0.5rem" }}>© {new Date().getFullYear()}, Built By Joel White</p>
+              <p style={{ color:"white", textAlign:"center", padding:"100px 0px 0px 0px", fontSize:"0.5rem" }}>© {new Date().getFullYear()}, Built By Joel White</p>
             </Grid>
 
           </Grid>
