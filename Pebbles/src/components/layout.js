52586e179a2d7b310b10322e0202951857bd4144
joelContractor <87203562+joelContractor@users.noreply.github.com>
2022-04-25 23:35:54 +0100

    Update layout.js

--- Pebbles/src/components/layout.js
+++ Pebbles/src/components/layout.js
@@ -38,124 +38,140 @@ const Layout = ({ children }) => {
         
       </div>
 
-      <div style={{ marginTop: `6rem`, background:"rgb(26, 36, 33) none repeat scroll 0% 0%", padding: `1.5rem 0 0 0`, width: '100%' }} >
+      <div style={{ marginTop: `6rem`, background:"rgb(26, 36, 33) none repeat scroll 0% 0%", padding: `1.5rem 0 1.5rem 0`, width: '100%' }} >
         
-      <div style={{ margin: `0 auto`, maxWidth: 800, padding: `0 1.0875rem 1.45rem`, color:"#adadad", fontSize:"0.8rem"}} >
-          <Grid container direction="row" spacing={2} style={{ paddingTop:"60px"}}>
+        <div style={{ margin: `0 auto`, maxWidth: 800, padding: `0 1.0875rem 1.45rem`, color:"#adadad", fontSize:"0.8rem"}} >
+            <Grid container direction="row" spacing={2} style={{ paddingTop:"60px"}}>
 
-            <Grid item xs={12} md={6} style={{ textAlign:"left", fontSize:"0.7rem" }}>
-              <h4 style={{ color:"white", fontWeight:"normal", width:"90%", margin:"0 auto", paddingBottom:"2rem", fontSize:"0.8rem"}}>Participate</h4>
-              <p style={{ fontFamily:"Arial", width:"90%", margin:"0 auto", paddingBottom:"2rem"}}>We aim to collate and help highlight diverse talent in the film industry. Be found on this site free! We ONLY feature publicly available contact details used for business (numbers/emails to agents or self-promoting talent). We are not an agency, any contact will be direct from production company to agent/talent.</p>
-            </Grid>
+              <Grid item xs={12} md={6} style={{ textAlign:"left", fontSize:"0.7rem" }}>
+                <h4 style={{ color:"white", fontWeight:"normal", width:"90%", margin:"0 auto", paddingBottom:"2rem", fontSize:"0.8rem"}}>Participate</h4>
+                <p style={{ fontFamily:"Arial", width:"90%", margin:"0 auto", paddingBottom:"2rem"}}>We aim to collate and help highlight diverse talent in the film industry. Be found on this site free! We ONLY feature publicly available contact details used for business (numbers/emails to agents or self-promoting talent). We are not an agency, any contact will be direct from production company to agent/talent.</p>
+              </Grid>
 
-            <Grid item xs={12} md={6} style={{ textAlign:"left", fontSize:"0.7rem" }}>
-              <h4 style={{ color:"white", fontWeight:"normal", width:"90%", margin:"0 auto", paddingBottom:"2rem", fontSize:"0.8rem"}}>Be Aware</h4>
-              <p style={{ fontFamily:"Arial", width:"90%", margin:"0 auto", paddingBottom:"2rem"}}>Under 18's without representation to handle queries are not permitted to be featured on this site. All data featured is publically available. We do not store any private user details. We do not process any data for any reason. Talent featured can request removal or modification of their profile at any time.</p>
-            </Grid>
+              <Grid item xs={12} md={6} style={{ textAlign:"left", fontSize:"0.7rem" }}>
+                <h4 style={{ color:"white", fontWeight:"normal", width:"90%", margin:"0 auto", paddingBottom:"2rem", fontSize:"0.8rem"}}>Be Aware</h4>
+                <p style={{ fontFamily:"Arial", width:"90%", margin:"0 auto", paddingBottom:"2rem"}}>Under 18's without representation to handle queries are not permitted to be featured on this site. All data featured is publically available. We do not store any private user details. We do not process any data for any reason. Talent featured can request removal or modification of their profile at any time.</p>
+              </Grid>
 
-            <Grid item xs={12} md={6} style={{ textAlign:"left", fontSize:"0.7rem" }}>
-              <h4 style={{ color:"white", fontWeight:"normal", width:"90%", margin:"0 auto", paddingBottom:"2rem", fontSize:"0.8rem"}}>Writer?</h4>
-              <p style={{ fontFamily:"Arial", width:"90%", margin:"0 auto", paddingBottom:"2rem"}}>We feature articles written by diverse writers, performers and crew from the TV, Film and Theatre industries - You. Get in touch about any issues you'd like to see discussed, or have your say internationally by publishing your very own article on diversecrew.com!</p>
-            </Grid>
+              <Grid item xs={12} md={6} style={{ textAlign:"left", fontSize:"0.7rem" }}>
+                <h4 style={{ color:"white", fontWeight:"normal", width:"90%", margin:"0 auto", paddingBottom:"2rem", fontSize:"0.8rem"}}>Writer?</h4>
+                <p style={{ fontFamily:"Arial", width:"90%", margin:"0 auto", paddingBottom:"2rem"}}>We feature articles written by diverse writers, performers and crew from the TV, Film and Theatre industries - You. Get in touch about any issues you'd like to see discussed, or have your say internationally by publishing your very own article on diversecrew.com!</p>
+              </Grid>
 
-            <Grid item xs={12} style={{ paddingBottom:"0rem", textAlign:"left", fontSize:"0.7rem", borderBottom:"0px dotted white", marginLeft:"55px", marginRight:"45px"  }}>
-            </Grid>
+              <Grid item xs={12} style={{ paddingBottom:"0rem", textAlign:"left", fontSize:"0.7rem", borderBottom:"0px dotted white", marginLeft:"105px", marginRight:"95px"  }}>
+              </Grid>
 
-            <Grid item xs={2} style={{ textAlign:"center", marginTop:"0rem", paddingTop:"4rem", paddingBottom:"4rem"  }}>
-              <h5 style={{ color:"white", fontWeight:"normal", fontSize:"0.6rem" }}>Follow</h5>
-              <a aria-label={`Follow Us`} href={`https://twitter.com/Diverse_Crew`} target="_blank" rel="noreferrer">
-                                      
-                  <StaticImage
-                      src="../images/twt.png"
-                      width={55}
-                      quality={95}
-                      formats={["AUTO", "WEBP", "AVIF"]}
-                      alt="Text Us Link"
-                      className="contactLinkImage"
-                  />
-              </a>
-            </Grid>
-
-            <Grid item xs={2} style={{ textAlign:"center", marginTop:"0rem", paddingTop:"4rem", paddingBottom:"4rem"  }} >
-              <h5 style={{ color:"white", fontWeight:"normal", fontSize:"0.6rem" }}>Friend</h5>
-              <a aria-label={`Befriend Us`} href={`https://www.facebook.com/profile.php?id=100079784120636`} target="_blank" rel="noreferrer">
-                                      
-                  <StaticImage
-                      src="../images/fbk.png"
-                      width={55}
-                      quality={95}
-                      formats={["AUTO", "WEBP", "AVIF"]}
-                      alt="Email Us Link"
-                      className="contactLinkImage"
-                  />
-                  
-              </a>
-            </Grid>
-
-            <Grid item xs={2} style={{ textAlign:"center", marginTop:"0rem", paddingTop:"4rem", paddingBottom:"4rem" }}>  
-              <h5 style={{ color:"white", fontWeight:"normal", fontSize:"0.6rem" }}>Link</h5>
-              <a aria-label={`Link Us`} href={`https://www.linkedin.com/company/81362179/admin/`} target="_blank" rel="noreferrer">
-                                      
-                  <StaticImage
-                      src="../images/li.png"
-                      width={55}
-                      quality={95}
-                      formats={["AUTO", "WEBP", "AVIF"]}
-                      alt="Call Us Link"
-                      className="contactLinkImage"
-                  />
-              </a>
+              
 
             </Grid>
 
-            <Grid item xs={2} style={{ textAlign:"center", marginTop:"0rem", paddingTop:"4rem", paddingBottom:"4rem"  }}>
-              <h5 style={{ color:"white", fontWeight:"normal", fontSize:"0.6rem" }}>Text</h5>
-              <a aria-label={`Send us a text`} href={`sms:07375510650`}>
-                                      
-                  <StaticImage
-                      src="../images/txr.png"
-                      width={55}
-                      quality={95}
-                      formats={["AUTO", "WEBP", "AVIF"]}
-                      alt="Text Us Link"
-                      className="contactLinkImage"
-                  />
-              </a>
+        </div>
+
+      </div>
+
+      <div style={{ marginTop: `0rem`, background:"#f1f1f1", padding: `0rem 0 0 0`, width: '100%' }} >
+        
+        <div style={{ margin: `0 auto`, maxWidth: 800, padding: `0 1.0875rem 0rem`, color:"#adadad", fontSize:"0.8rem"}} >
+            <Grid container direction="row" spacing={2} style={{ paddingTop:"1rem"}}>
+
+              <Grid item xs={12} style={{ paddingBottom:"0rem", textAlign:"left", fontSize:"0.7rem", borderBottom:"0px dotted white", marginLeft:"105px", marginRight:"95px"  }}>
+              </Grid>
+
+              <Grid item xs={2} style={{ textAlign:"center", marginTop:"0rem", paddingTop:"2rem", paddingBottom:"4rem"  }}>
+                <h5 style={{ color:"hsla(0, 0%, 0%, 0.6)", fontWeight:"bold", fontSize:"0.6rem" }}>Follow</h5>
+                <a aria-label={`Follow Us`} href={`https://twitter.com/Diverse_Crew`} target="_blank" rel="noreferrer">
+                                        
+                    <StaticImage
+                        src="../images/twt.png"
+                        width={55}
+                        quality={95}
+                        formats={["AUTO", "WEBP", "AVIF"]}
+                        alt="Text Us Link"
+                        className="contactLinkImage"
+                    />
+                </a>
+              </Grid>
+
+              <Grid item xs={2} style={{ textAlign:"center", marginTop:"0rem", paddingTop:"2rem", paddingBottom:"4rem"  }} >
+                <h5 style={{ color:"hsla(0, 0%, 0%, 0.6)", fontWeight:"bold", fontSize:"0.6rem" }}>Friend</h5>
+                <a aria-label={`Befriend Us`} href={`https://www.facebook.com/profile.php?id=100079784120636`} target="_blank" rel="noreferrer">
+                                        
+                    <StaticImage
+                        src="../images/fbk.png"
+                        width={55}
+                        quality={95}
+                        formats={["AUTO", "WEBP", "AVIF"]}
+                        alt="Email Us Link"
+                        className="contactLinkImage"
+                    />
+                    
+                </a>
+              </Grid>
+
+              <Grid item xs={2} style={{ textAlign:"center", marginTop:"0rem", paddingTop:"2rem", paddingBottom:"4rem" }}>  
+                <h5 style={{ color:"hsla(0, 0%, 0%, 0.6)", fontWeight:"bold", fontSize:"0.6rem" }}>Link</h5>
+                <a aria-label={`Link Us`} href={`https://www.linkedin.com/company/81362179/admin/`} target="_blank" rel="noreferrer">
+                                        
+                    <StaticImage
+                        src="../images/li.png"
+                        width={55}
+                        quality={95}
+                        formats={["AUTO", "WEBP", "AVIF"]}
+                        alt="Call Us Link"
+                        className="contactLinkImage"
+                    />
+                </a>
+
+              </Grid>
+
+              <Grid item xs={2} style={{ textAlign:"center", marginTop:"0rem", paddingTop:"2rem", paddingBottom:"4rem"  }}>
+                <h5 style={{ color:"hsla(0, 0%, 0%, 0.6)", fontWeight:"bold", fontSize:"0.6rem" }}>Text</h5>
+                <a aria-label={`Send us a text`} href={`sms:07375510650`}>
+                                        
+                    <StaticImage
+                        src="../images/txr.png"
+                        width={55}
+                        quality={95}
+                        formats={["AUTO", "WEBP", "AVIF"]}
+                        alt="Text Us Link"
+                        className="contactLinkImage"
+                    />
+                </a>
+              </Grid>
+
+              <Grid item xs={2} style={{ textAlign:"center", marginTop:"0rem", paddingTop:"2rem", paddingBottom:"4rem"  }} >
+                <h5 style={{ color:"hsla(0, 0%, 0%, 0.6)", fontWeight:"bold", fontSize:"0.6rem" }}>Email</h5>
+                <a aria-label={`Email us`} href={`mailto:diversecrew.com@gmail.com?&subject=DiverseCrewEnquiry&body=Hi Diverse Crew, I'd like a free profile`}>
+                                        
+                    <StaticImage
+                        src="../images/emr.png"
+                        width={55}
+                        quality={95}
+                        formats={["AUTO", "WEBP", "AVIF"]}
+                        alt="Email Us Link"
+                        className="contactLinkImage"
+                    />
+                    
+                </a>
+              </Grid>
+
+              <Grid item xs={2} style={{ textAlign:"center", marginTop:"0rem", paddingTop:"2rem", paddingBottom:"4rem" }}>  
+                <h5 style={{ color:"hsla(0, 0%, 0%, 0.6)", fontWeight:"bold", fontSize:"0.6rem" }}>Call</h5>
+                <a aria-label={`Call Us`} href={`tel:07375510650`}>
+                                        
+                    <StaticImage
+                        src="../images/car.png"
+                        width={55}
+                        quality={95}
+                        formats={["AUTO", "WEBP", "AVIF"]}
+                        alt="Call Us Link"
+                        className="contactLinkImage"
+                    />
+                </a>
+
+              </Grid>
+
             </Grid>
-
-            <Grid item xs={2} style={{ textAlign:"center", marginTop:"0rem", paddingTop:"4rem", paddingBottom:"4rem"  }} >
-              <h5 style={{ color:"white", fontWeight:"normal", fontSize:"0.6rem" }}>Email</h5>
-              <a aria-label={`Email us`} href={`mailto:diversecrew.com@gmail.com?&subject=DiverseCrewEnquiry&body=Hi Diverse Crew, I'd like a free profile`}>
-                                      
-                  <StaticImage
-                      src="../images/emr.png"
-                      width={55}
-                      quality={95}
-                      formats={["AUTO", "WEBP", "AVIF"]}
-                      alt="Email Us Link"
-                      className="contactLinkImage"
-                  />
-                  
-              </a>
-            </Grid>
-
-            <Grid item xs={2} style={{ textAlign:"center", marginTop:"0rem", paddingTop:"4rem", paddingBottom:"4rem" }}>  
-              <h5 style={{ color:"white", fontWeight:"normal", fontSize:"0.6rem" }}>Call</h5>
-              <a aria-label={`Call Us`} href={`tel:07375510650`}>
-                                      
-                  <StaticImage
-                      src="../images/car.png"
-                      width={55}
-                      quality={95}
-                      formats={["AUTO", "WEBP", "AVIF"]}
-                      alt="Call Us Link"
-                      className="contactLinkImage"
-                  />
-              </a>
-
-            </Grid>
-
-          </Grid>
 
           
           
