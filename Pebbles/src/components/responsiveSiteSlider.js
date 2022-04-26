bd2ca0f33f2258be6aab4b005e4feb6a34685339
joelContractor <87203562+joelContractor@users.noreply.github.com>
2022-04-26 04:15:33 +0100

    card title sizing for mobile

--- Pebbles/src/components/responsiveSiteSlider.js
+++ Pebbles/src/components/responsiveSiteSlider.js
@@ -42,7 +42,7 @@ const StyledHistorySwiperGrid = styled(Grid, {
     ".cardContentPara": { fontFamily: 'Arial', fontSize:"0.8rem", color: "#808080", marginBottom:"0px", maxHeight:"80px", lineHeight:"1rem" },
     ".MuiCardContent-root:last-child": { paddingBottom:"25px" },
     ".buttonContainer": { display:"flex", justifyContent:"flex-end", marginTop: "0px" },
-    ".cardTitle": { marginBottom:"0.01rem", color: `#141414`, fontSize:"0.8rem" },
+    ".cardTitle": { marginBottom:"0.01rem", color: `#141414`, fontSize:"0.9rem" },
     ".safariRounded": { willChange:"transform", borderTopLeftRadius: "20px", borderTopRightRadius: "20px", overflow:"hidden", webkitBackfaceVisibility: "hidden", mozBackfaceVisibility: "hidden", webkitTransform: "translate3d(0, 0, 0)", mozTransform: "translate3d(0, 0, 0)", webkitMaskImage: "webkitRadialGradient(white, black)" }
     
 
