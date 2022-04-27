869ff20d321ba7d5d3051f460b5254d696f7b0b1
joelContractor <87203562+joelContractor@users.noreply.github.com>
2022-04-27 11:45:47 +0100

    Update index.js

--- Pebbles/src/pages/index.js
+++ Pebbles/src/pages/index.js
@@ -48,18 +48,6 @@ function IndexPage({data}) {
 
     { !matchesUnder && <ArticleGallery  data={ data.allDatoCmsDiversityarticle.edges } /> }
     { matchesUnder && <ResponsiveArticleSlider  data={ data.allDatoCmsDiversityarticle.edges } /> }
-    <form name="Contact Form" method="POST" data-netlify="true">
-      <input type="hidden" name="form-name" value="Contact Form" />
-      <div>
-        <label>Your Email:</label>
-        <input type="email" name="email" />
-      </div>
-      <div>
-        <label>Message:</label>
-        <textarea name="message" />
-      </div>
-      <button type="submit">Send</button>
-    </form>
   
   </Layout>
   
