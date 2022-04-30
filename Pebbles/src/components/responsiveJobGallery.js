6ef28949952e6a2a4e6042583f363d61507758af
joelContractor <87203562+joelContractor@users.noreply.github.com>
2022-04-30 19:11:48 +0100

    Update responsiveJobGallery.js
    clean up

--- Pebbles/src/components/responsiveJobGallery.js
+++ Pebbles/src/components/responsiveJobGallery.js
@@ -86,7 +86,7 @@ function ResponsiveJobSlider({ data }) {
                         
 
                         
-                        <>
+                        
                         <SwiperSlide key={dats.node.jobTitle} >
 
                             <Grid key={dats.node.jobTitle} item xs={12} state={data}>
@@ -94,12 +94,12 @@ function ResponsiveJobSlider({ data }) {
                                 <Card className="infoCard">
 
                                     <Grid item xs={12} className="safariRounded">
-                                        <GatsbyImage className="cardMediaArea" image={getImage(dats.node.jobImage.gatsbyImageData)} alt={dats.node.articletitle}/>
+                                        <GatsbyImage className="cardMediaArea" image={getImage(dats.node.jobImage.gatsbyImageData)} alt={dats.node.jobTitle}/>
                                     </Grid>
                                     <Grid item xs={12} onClick={showResForm}>
-                                    <CardContent
-                                        className="cardContentArea"
-                                    >
+                                        <CardContent
+                                            className="cardContentArea"
+                                        >
                                     
                                             <Grid item xs={12}>
                                                 <p className="cardTitle">
@@ -137,53 +137,53 @@ function ResponsiveJobSlider({ data }) {
                                             </Grid>
                                     
                                         
-                                    </CardContent>
+                                        </CardContent>
                                     </Grid>
 
                                 </Card>               
                             </Grid>
-                            <Grid xs={12} style={{textAlign:"center", display:show === 'none'? `none`: `block`}}>
+                            <Grid item xs={12} style={{textAlign:"center", display:show === 'none'? `none`: `block`}}>
 
-                            
+                                
 
-                        <form className="formWidth" name="Contact Form" method="POST" netlify-honeypot="entryField" data-netlify="true">
-                            <input type="hidden" name="form-name" value="Contact Form" />
-                            <Grid xs={12}>
-                                <h3 className="white">Apply</h3>
-                            </Grid>
-                            <Grid xs={12} className="left">
-                                <label className="white" htmlFor="email">Your Email:<input className="monowidth" placeholder="you@gmail.com" type="email" name="email" id="email" required/></label>
-                                
-                            </Grid>
-                            <Grid xs={12} style={{ display:"none" }}>
-                                <label className="white" htmlFor="entryField" >Your Key:<input className="monowidth" placeholder="your-secret-userkey" type="text" name="entryField" id="entryField" /></label>
-                                
-                            </Grid>
-                            <Grid xs={12} className="left">
-                                <label htmlFor="profile-url" className="white">Your Profile URL:<input className="monowidth" placeholder="you@imdb.com" type="text" name="profile-url" id="profile-url" required/></label>
-                                
-                            </Grid>
-                            <Grid xs={12} className="left">
-                                <label className="white" htmlFor="message" >Message:<textarea className="monowidth" placeholder="Hello, my name is example-name, I am interested in your advertised job role." name="message" id="message" required/></label>
-                                
-                            </Grid>
-                            <Grid xs={12} style={{ marginBottom:"1rem", textAlign:"center" }}>
-                                <p className="monowidth white" style={{ marginTop:"0.3rem" }}>Please be patient while awaiting a response.</p>
-                            </Grid>
-                            <Grid xs={12} style={{display:"none"}}>
-                                <label htmlFor="employer-email" >Employer Email:</label>
-                                <input className="monowidth" type="password" name="employer-email" id="employer-email" value={dats.node.employerEmail} />
-                            </Grid>
-                            <Grid xs={12} style={{textAlign:"center", marginBottom:"0.3rem"}}>
-                                <Button style={{ fontWeight:"bold", borderRadius:"50px", padding:"1.5rem 1rem", background:"white", color:"rgb(26, 36, 33)", fontSize:"0.6rem" }} type="submit">Send</Button>
-                            </Grid>
-                            
-                        </form>
+                                <form className="formWidth" name="Contact Form" method="POST" netlify-honeypot="entryField" data-netlify="true">
+                                    <input type="hidden" name="form-name" value="Contact Form" />
+                                    <Grid item xs={12}>
+                                        <h3 className="white">Apply</h3>
+                                    </Grid>
+                                    <Grid item xs={12} className="left">
+                                        <label className="white" htmlFor="email">Your Email:<input className="monowidth" placeholder="you@gmail.com" type="email" name="email" id="email" required/></label>
+                                        
+                                    </Grid>
+                                    <Grid item xs={12} style={{ display:"none" }}>
+                                        <label className="white" htmlFor="entryField" >Your Key:<input className="monowidth" placeholder="your-secret-userkey" type="text" name="entryField" id="entryField" /></label>
+                                        
+                                    </Grid>
+                                    <Grid item xs={12} className="left">
+                                        <label htmlFor="profile-url" className="white">Your Profile URL:<input className="monowidth" placeholder="you@imdb.com" type="text" name="profile-url" id="profile-url" required/></label>
+                                        
+                                    </Grid>
+                                    <Grid item xs={12} className="left">
+                                        <label className="white" htmlFor="message" >Message:<textarea className="monowidth" placeholder="Hello, my name is example-name, I am interested in your advertised job role." name="message" id="message" required/></label>
+                                        
+                                    </Grid>
+                                    <Grid item xs={12} style={{ marginBottom:"1rem", textAlign:"center" }}>
+                                        <p className="monowidth white" style={{ marginTop:"0.3rem" }}>Please be patient while awaiting a response.</p>
+                                    </Grid>
+                                    <Grid item xs={12} style={{display:"none"}}>
+                                        <label htmlFor="employer-email" >Employer Email:</label>
+                                        <input className="monowidth" type="password" name="employer-email" id="employer-email" value={dats.node.employerEmail} readOnly/>
+                                    </Grid>
+                                    <Grid item xs={12} style={{textAlign:"center", marginBottom:"0.3rem"}}>
+                                        <Button style={{ fontWeight:"bold", borderRadius:"50px", padding:"1.5rem 1rem", background:"white", color:"rgb(26, 36, 33)", fontSize:"0.6rem" }} type="submit">Send</Button>
+                                    </Grid>
+                                    
+                                </form>
 
 
-                        </Grid>
+                            </Grid>
                         </SwiperSlide>
-                        </>
+                        
                     ))}
                     
                 </Swiper>
