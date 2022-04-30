bf4977742ca76ef06bf8300136e8a95792896fe9
joelContractor <87203562+joelContractor@users.noreply.github.com>
2022-04-30 18:12:52 +0100

    Update responsiveJobGallery.js
    swiper buttons featured outside of slider

--- Pebbles/src/components/responsiveJobGallery.js
+++ Pebbles/src/components/responsiveJobGallery.js
@@ -91,7 +91,7 @@ function ResponsiveJobSlider({ data }) {
 
                             <Grid key={dats.node.jobTitle} item xs={12} state={data}>
                         
-                                <Card className="infoCard">
+                                <Card style={{ width:"80%" }} className="infoCard">
 
                                     <Grid item xs={12} className="safariRounded">
                                         <GatsbyImage className="cardMediaArea" image={getImage(dats.node.jobImage.gatsbyImageData)} alt={dats.node.articletitle}/>
